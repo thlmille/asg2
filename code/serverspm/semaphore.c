0e91ee6886a6a33df61ed20277b76b31c50aef93
Thomas Miller <thlmille@ucsc.edu>
2012-05-05 16:35:39 -0700

    lincoln's semaphore implementation

--- code/serverspm/semaphore.c
+++ code/serverspm/semaphore.c
@@ -1,40 +1,118 @@
+#include "pm.h"
 #include <sys/queue.h>
 #include <errno.h>
-#include "semaphore.h"
+#include <lib.h>
+#include <string.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include "param.h"
+#include "mproc.h"
 
-int seminit(int sem,int value){
-  if (sems_in_use > 99){
-    return EAGAIN;
-  }
-  if (sem > 1000 || sem < 0){
+typedef struct sem{
+  int id;
+  int val;
+  int proc[1000];
+}sem;
+
+PRIVATE sem* sem_array[101];
+//memset(&sem_array,NULL,100*sizeof(sem));
+
+PUBLIC int do_seminit(){
+  int sema=m_in.m1_i1; //first value to pass in
+  int value = m_in.m1_i2; //second value to pass in
+  printf("sem : %d, value: %d\n",sema,value);
+  if (sema > 1000 || sema < 0){
     return EINVAL;
   }
-  if (check_sem(sem) != 0){
-    return EEXIST;
-  } else {
-    if (value == 0){ //value needs to be non-zero pos.
-      return -1;
-    } else {
-      sem_list[sem]->block = value; //how many processes are allowed before blocking
-      sem_list[sem]->current = 0; //tells how many processes are currently waiting
-      sem_list[sem]->id = sem; //not needed
-      LIST_HEAD(sem_l,sem) sem_head;
-      LIST_INIT(&sem_head);
+  if (sema == 0){
+    int i;
+    for(i=1;i<101;i++){
+      if (sem_array[i] == NULL){
+        sema = i;
+      } 
     }
+    if (sema == 0){
+      return EAGAIN;
+    }
+  }
+  if (sem_array[sema] == NULL){
+    sem *a;
+    a->id = sema;
+    a->val = value;
+    memset(&a->proc,0,100*sizeof(int));
+    memcpy(sem_array[sema],&a,sizeof(sem));
+  } else {
+    printf("value already exists\n");
+    return EEXIST;
+  }
+
+  return 0;
+}
+
+PUBLIC int do_semvalue(){
+  int sem=m_in.m1_i1;
+  printf("sem: %d\n",sem);
+  if (sem_array[sem] == NULL){
+    return 0x8000000;
+  } else {
+    printf("returning : %d\n" ,sem_array[sem]->val);
+    return sem_array[sem]->val;
   }
 }
 
-int semvalue(int sem){
-  
+PUBLIC int do_semup(){
+  int sem=m_in.m1_i1;
+  if (sem_array[sem] != NULL){
+    sem_array[sem]->val += 1;
+    if (sem_array[sem]->val > 1000){
+      return EOVERFLOW;
+    }
+  } else {
+    return 1;
+  }
+  if (sem_array[sem]->val <= 0){
+    int process = sem_array[sem]->proc[0];
+    mp[process].mp_flags = REPLY;
+    memcpy(&sem_array,&sem_array+1,99*sizeof(int));
+
+    //do_(); //this is where we wake up waiting proc
+  }
+  //printf("sem: %d\n",sem);
+  return 0;
 }
 
-int check_sem(int sem){
-  if(sem_list[sem] != 0){ //zero means it is not in use
+PUBLIC int do_semdown(){
+  int sem=m_in.m1_i1;
+  if (sem_array[sem] != NULL){
+    sem_array[sem]->val -= 1;
+    if (sem_array[sem]->val < 1000){
+      return EOVERFLOW;
+    }
+  } else {
     return 1;
+  }
+  if (sem_array[sem]->val < 0){
+    mp->mp_flags = PAUSE;
+    int i = 0;
+    while(i<100){
+      if (sem_array[sem]->proc[i] == 0){
+        sem_array[sem]->proc[i] = mp->mp_pid;
+        i = 101;
+      }
+    }
   }
   return 0;
 }
 
-int semup(int sem){}
-int semdown(int sem){}
-int semfree(int sem){}
+PUBLIC int do_semfree(){
+  int sem=m_in.m1_i1;
+  if (sem_array[sem] != NULL){
+    while (sem_array[sem]->val < 0){
+      do_semup(sem);
+    }
+    free(sem_array[sem]);
+    sem_array[sem] = NULL;
+  }
+  printf("sem: %d\n",sem);
+  return 0;
+}
