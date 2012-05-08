c6a72e81a9c5ddd1ffd4ae6273a432329effd98d
Thomas Miller <thlmille@ucsc.edu>
2012-05-07 19:15:18 -0700

    update semtest 3

--- code/semtests/semtest3.c
+++ code/semtests/semtest3.c
@@ -5,8 +5,11 @@ int main (int argc, char **argv) {
   int b = seminit (5, 0);
   int c = semfree (5);
   int d = seminit (5, 0);
+  int e = semfree (5);
   printf ("a = %d\n", a);
   printf ("b = %d\n", b);
   printf ("c = %d\n", c);
   printf ("d = %d\n", d);
+  printf ("e = %d\n", e);
+  return 0;
 }
