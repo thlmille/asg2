7151e8aea6f31cfb8458c71c18a5de72fb5e7590
Thomas Miller <thlmille@ucsc.edu>
2012-05-07 18:40:58 -0700

    update semtest1

--- code/semtests/semtest1.c
+++ code/semtests/semtest1.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 
 int main (int argc, char **argv) {
-  int derp = seminit(0,0);
-  printf ("semval = %d", derp);
+  int foo = seminit(1,0);
+  printf ("semval = %d\n", foo);
+  int bar = seminit(1,0);
+  if (bar != 1) printf("bar rejected\n");
   return 0;
 }
