7530e4f7a1d619782d334ec12f07faa0ff028191
Thomas Miller <thlmille@ucsc.edu>
2012-05-10 23:12:53 -0700

    debug statements modified by lincoln

--- code/serverspm/semaphore.c
+++ code/serverspm/semaphore.c
@@ -8,7 +8,13 @@
 #include "param.h"
 #include "mproc.h"
 
+#define DEBUG 1
 
+#ifdef DEBUG
+#define PRINT_DEBUG(...) fprintf(stderr, __VA_ARGS__)
+#else
+#define PRINT_DEBUG(...)
+#endif
 
 typedef struct sem{
   int id;
@@ -17,20 +23,14 @@ typedef struct sem{
 } sem;
 
 PRIVATE sem sem_array[100];
-int debug = 1;
 int nsems = 0;
 
 /* Array of numbers to use when we need to assign an id */
 int used_nums[101];
 
-PRIVATE void debug_message (int flag, char *str) {
-  if (flag) {
-    printf ("%s\n", str);
-  }
-}
 
 PRIVATE void initialize () {
-  debug_message(debug, "in initialize");
+  PRINT_DEBUG("in initialize\n");
   int i;
   for (i = 0; i < 100; ++i) {
     sem_array[i].id = 0;
@@ -45,32 +45,30 @@ PUBLIC int do_seminit () {
 
   if (nsems == 0) initialize();
 
-  if (debug) {
-    printf ("in seminit\n");
-    printf ("user_id = %d\n", user_id);
-    printf ("value = %d\n", value);
-    printf ("nsems = %d\n", nsems);
-  }
+  PRINT_DEBUG ("in seminit\n");
+  PRINT_DEBUG ("user_id = %d\n", user_id);
+  PRINT_DEBUG ("value = %d\n", value);
+  PRINT_DEBUG ("nsems = %d\n", nsems);
 
   if (nsems == 99) {
-    debug_message (debug, "semaphore limit exceeded");
+    PRINT_DEBUG("semaphore limit exceeded\n");
     return -1 * EAGAIN;
   }
 
   if (user_id < 0) {
-    debug_message (debug, "id less than 0");
+    PRINT_DEBUG("id less than 0\n");
     return -1 * EINVAL;
   }
   
   if (value > 1000 || value < -1000) {
-    debug_message (debug, "value out of range");
+    PRINT_DEBUG("value out of range\n");
     return -1 * EINVAL;
   }
 
   /* If given ID is 0, assign an id number */
   if (user_id == 0) {
     /* Find open slot in used_nums array */
-    debug_message (debug, "assigning value");
+    PRINT_DEBUG("assigning value\n");
     int i;
     for (i = 1; i < 101; ++i) {
       if (!used_nums[i]) {
@@ -82,11 +80,11 @@ PUBLIC int do_seminit () {
   }
 
   /* Check if semaphore already exists, if not, create it */
-  debug_message (debug, "checking if semaphore exists");
+  PRINT_DEBUG("checking if semaphore exists\n");
   int i;
   for (i = 0; i < 100; ++i) {
     if (user_id == sem_array[i].id) {
-      debug_message (debug, "semaphore already exists");
+      PRINT_DEBUG("semaphore already exists\n");
       return -1 * EEXIST;
     }
   }
@@ -101,11 +99,11 @@ PUBLIC int do_seminit () {
   }
   /* This should never happen */
   if (open == 100) {
-    debug_message (debug, "shit is fucked");
+    PRINT_DEBUG("shit is fucked\n");
     return -1 *EAGAIN;
   }
   /* Initialize semaphore */
-  debug_message (debug, "initialize semaphore");
+  PRINT_DEBUG("initialize semaphore\n");
   sem_array[open].id = user_id;
   sem_array[open].val = value;
   int j;
@@ -119,27 +117,25 @@ PUBLIC int do_seminit () {
 
 PUBLIC int do_semup () {
   int sem_id = m_in.m1_i1;
-  if (debug) {
-    printf("in semup\n sem_id = %d\n", sem_id);
-  }
+  PRINT_DEBUG("in semup\n sem_id = %d\n", sem_id);
   /* Find the semaphore, error if it doesn't exist */
   int i;
   for (i = 0; i < 100; ++i) {
     if (sem_array[i].id == sem_id) break;
   }
   if (i == 100) {
-    debug_message (debug, "semaphore does not exist\n");
+    PRINT_DEBUG("semaphore does not exist\n");
     return -1 * EEXIST;
   }
   int sem_index = i;
   /* Make sure value is in range */
   if (sem_array[sem_index].val < -1000) {
-    debug_message (debug, "value < -1000");
+    PRINT_DEBUG("value < -1000\n");
     return EOVERFLOW;
   }
   /* If value is less than 0, run the first process waiting */
   if (sem_array[sem_index].val < 0) {
-    debug_message (debug, "value less than 0");
+    PRINT_DEBUG("value less than 0\n");
     sem_array[sem_index].val++;
     /* Take first process, move other processes over */
     int process = sem_array[sem_index].proc[0];
@@ -147,34 +143,28 @@ PUBLIC int do_semup () {
     for (j = 1; j < 1000; ++j) {
       sem_array[sem_index].proc[j-1] = sem_array[sem_index].proc[j];
     }
-    if (debug) {
-      printf ("waking process %d\n", process);
-    }
+    PRINT_DEBUG("waking process %d\n", process);
     setreply (process, OK);
   }
   return 0;
 }
 PUBLIC int do_semdown () {
   int sem_id = m_in.m1_i1;
-  if (debug) {
-    printf("in semvalue\nsem_id = %d\n", sem_id);
-  }
+  PRINT_DEBUG("in semvalue\nsem_id = %d\n", sem_id);
   /* Find the semaphore, error if it doesn't exist */
   int i;
   for (i = 0; i < 100; ++i) {
     if (sem_array[i].id == sem_id) break;
   }
   if (i == 100) {
-    debug_message (debug, "semaphore does not exist\n");
+    PRINT_DEBUG("semaphore does not exist\n");
     return -1 * EEXIST;
   }
   int sem_index = i;
   /* Make sure value is in range */
   if (sem_array[sem_index].val < -1000) {
-    if (debug) {
-      printf ("value = %d\n", sem_array[sem_index].val);
-    }
-    debug_message (debug, "value < -1000");
+    PRINT_DEBUG ("value = %d\n", sem_array[sem_index].val);
+    PRINT_DEBUG("value < -1000\n");
     return EOVERFLOW;
   }
   /* if value is greater than 0, decrement value */
@@ -187,17 +177,15 @@ PUBLIC int do_semdown () {
     int j;
     for (j = 0; j < 1000; ++j) {
       if (sem_array[sem_index].proc[j] == 0) {
-	if (debug) {
-	  printf ("process %d added to queue", who_p);
-	}
-	sem_array[sem_index].proc[j] = who_p;
-	break;
+	    PRINT_DEBUG ("process %d added to queue", who_p);
+	    sem_array[sem_index].proc[j] = who_p;
+	    break;
       }
     }
-    if (debug) {
-      printf ("process list\n");
+    if (DEBUG) {
+      PRINT_DEBUG("process list\n");
       for (i = 0; i < 5; ++i) {
-	printf ("%d: %d\n", i, sem_array[sem_index].proc[i]);
+	    printf ("%d: %d\n", i, sem_array[sem_index].proc[i]);
       }
     }
     return SUSPEND;
@@ -206,7 +194,7 @@ PUBLIC int do_semdown () {
 }
 PUBLIC int do_semvalue () {
   int sem_id = m_in.m1_i1;
-  if (debug) {
+  if (DEBUG) {
     printf("in semvalue\n sem_id = %d\n", sem_id);
   }
   /* Find the semaphore, error if it doesn't exist */
@@ -215,7 +203,7 @@ PUBLIC int do_semvalue () {
     if (sem_array[i].id == sem_id) break;
   }
   if (i == 100) {
-    debug_message (debug, "semaphore does not exist\n");
+    PRINT_DEBUG("semaphore does not exist\n");
     return -1 * EEXIST;
   }
   return sem_array[i].val;
@@ -223,16 +211,14 @@ PUBLIC int do_semvalue () {
 
 PUBLIC int do_semfree () {
   int sem_id = m_in.m1_i1;
-  if (debug) {
-    printf ("in semfree\n sem_id = %d\n", sem_id);
-  }
+  PRINT_DEBUG("in semfree\n sem_id = %d\n", sem_id);
   /* Find semaphore */
   int i;
   for (i = 0; i < 100; ++i) {
     if (sem_array[i].id == sem_id) break;
   }
   if (i == 100) {
-    debug_message (debug, "semaphore does not exist");
+    PRINT_DEBUG("semaphore does not exist\n");
     return -1 * EEXIST;
   }
   /* semup until all processes are freed */
